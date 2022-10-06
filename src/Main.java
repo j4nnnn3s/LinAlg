7c39a1445c6f066d4d689d01612c4e9772f64688
j4nnnn3s <j-grauer@gmx.de>
2022-10-06 12:23:02 +0200

    Update gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import linalg.SmartNum;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(
+                new SmartNum("0.1")
+        );
     }
 }
