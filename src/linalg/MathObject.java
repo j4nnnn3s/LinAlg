d093b650dd78fffc5e64ddf43ad4794377ed4b8e
j4nnnn3s <j-grauer@gmx.de>
2022-10-06 15:46:41 +0200

    Added MathObject

---
+++ src/linalg/MathObject.java
@@ -0,0 +1,30 @@
+package linalg;
+
+abstract class MathObject {
+
+    public MathObject(String s) {}
+
+    MathObject() {
+    }
+
+    public MathObject add(MathObject o) {
+        return this;
+    }
+
+    public MathObject mult(MathObject o) {
+        return this;
+    }
+
+    public MathObject divide(MathObject o) {
+        return this;
+    }
+
+    public MathObject subtract(MathObject o) {
+        return this;
+    }
+
+    public boolean equals(MathObject o) {
+        return this == o;
+    }
+
+}
