86cf5ed3c3c265df93b8955689320d763ab3de3a
j4nnnn3s <j-grauer@gmx.de>
2022-10-06 21:24:33 +0200

    Infinit solutions is solvable
    The implemented gauss algorithm can now solve matrices with infinite solutions.

--- src/linalg/MatrixManipulator.java
+++ src/linalg/MatrixManipulator.java
@@ -13,24 +13,44 @@ public class MatrixManipulator {
     public static TwoDMatrix solveGauss(TwoDMatrix matrix) {
         try {
             int maxDiag = Math.min(matrix.numberOfColumns() - 1, matrix.numberOfRows());
+            int movingFactor = 0; // It is possible, that the pivot might be 0. Thats why movingFactor
             for (int diagPosition = 0; diagPosition < maxDiag; diagPosition++) {
-                SmartNum pivot = matrix.get(diagPosition, diagPosition);
+                SmartNum pivot;
+                do {
+                    pivot = matrix.get(diagPosition, diagPosition + movingFactor);
+                    if(pivot.isZero() && movingFactor < matrix.numberOfColumns() - 3) movingFactor++;
+                    else break;
+                } while (pivot.isZero());
+                if (pivot.isZero()) continue;
+
                 for (int row = 0; row < matrix.numberOfRows(); row++) {
                     if (row == diagPosition) {
                         continue;
                     }
-                    SmartNum factor = new SmartNum(-1).mult(matrix.get(row, diagPosition)).divide(pivot);
+                    SmartNum factor = new SmartNum(-1).mult(matrix.get(row, diagPosition + movingFactor)).divide(pivot);
                     for (int column = 0; column < matrix.numberOfColumns(); column++) {
                         SmartNum currentManipulator = matrix.get(diagPosition, column).mult(factor);
                         SmartNum target = matrix.get(row, column);
                         target.add(currentManipulator);
                         matrix.set(row, column, target);
                     }
+                    if (matrix.isRowZero(row)) {
+                        matrix.deleteRow(row);
+                        return solveGauss(matrix);
+                    }
                 }
             }
             for (int diagPosition = 0; diagPosition < maxDiag; diagPosition++) {
-                SmartNum pivot = matrix.get(diagPosition, diagPosition);
-                matrix.set(diagPosition, diagPosition, new SmartNum(1));
+                SmartNum pivot;
+                movingFactor = 0;
+                do {
+                    pivot = matrix.get(diagPosition, diagPosition + movingFactor);
+                    if(pivot.isZero() && movingFactor < matrix.numberOfColumns() - 3) movingFactor++;
+                    else break;
+                } while (pivot.isZero());
+                if (pivot.isZero()) continue;
+
+                matrix.set(diagPosition, diagPosition + movingFactor, new SmartNum(1));
                 SmartNum lastElement = matrix.get(diagPosition, matrix.numberOfColumns() - 1);
                 lastElement.divide(pivot);
                 matrix.set(diagPosition, matrix.numberOfColumns() - 1, lastElement);
