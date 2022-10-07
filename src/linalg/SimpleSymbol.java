2ab0580a58952decd7b8f75203b19f7d845b00f0
j4nnnn3s <j-grauer@gmx.de>
2022-10-07 18:19:36 +0200

    Improved parseSimnpleSymbol
    Should now work with powers. Please test

--- src/linalg/SimpleSymbol.java
+++ src/linalg/SimpleSymbol.java
@@ -5,29 +5,73 @@ import java.util.regex.Pattern;
 
 public class SimpleSymbol {
 
-    public char symbol;
-    public SmartNum factor;
+    private char symbol;
+    private SmartNum factor;
+    private SmartNum power;
+
     // TODO: Fix that *[a-z][A-Z] without number in the beginning isn't allowed
-    private static final Pattern PATTERN = Pattern.compile("-?\\d*((\\.?|\\/?)\\d*)?\\s*\\*?\\s*[a-z]|[A-Z]");
+    private static final Pattern PATTERN = Pattern.compile(
+            "(-?\\d*((\\.?|\\/?)\\d*)?\\s*\\*?\\s*)(?:[a-z]|[A-Z])(?:\\s*\\^?-?\\d*((\\.?|\\/?)\\d*)?\\s*)"
+    );
 
     public SimpleSymbol(SmartNum factor, char symbol) {
         this.factor = factor;
         this.symbol = symbol;
+        this.power = new SmartNum(1);
+    }
+
+    public SimpleSymbol(SmartNum factor, char symbol, SmartNum power) {
+       this.factor = factor;
+       this.symbol = symbol;
+       this.power = power;
     }
 
     public SimpleSymbol(String s) {
         parseSimpleSymbol(s);
     }
 
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public SmartNum getFactor() {
+        return factor;
+    }
+
+    public SmartNum getPower() {
+        return power;
+    }
+
     private void parseSimpleSymbol(String s) {
-        Matcher matcher = PATTERN.matcher(s);
-        if(!matcher.matches()) {
+        s = s.replace(" ", "");
+        if(!validSimpleSymbol(s)) {
             throw new NumberFormatException("Not a valid symbol");
         }
         if (s.contains("*")) s = s.replace("*", "");
-        symbol = s.charAt(s.length() - 1);
+        for (String element : s.split("")) {
+            if (element.matches("[a-z]|[A-Z]"))  {
+                symbol = element.charAt(0);
+                s = s.replace(String.valueOf(symbol), "");
+                break;
+            }
+        }
         s = s.replace(String.valueOf(symbol), "");
-        factor = new SmartNum(s);
+        if (s.isEmpty()) {
+            factor = new SmartNum(1);
+            power = new SmartNum(1);
+        } else if (s.contains("^")) {
+            String elements[] = s.split("\\^");
+            factor = new SmartNum(elements[0]);
+            power = new SmartNum(elements[1]);
+        } else {
+            factor = new SmartNum(s);
+            power = new SmartNum(1);
+        }
+    }
+
+    public boolean validSimpleSymbol(String s) {
+        Matcher matcher = PATTERN.matcher(s);
+        return PATTERN.matcher(s).matches();
     }
 
     public SimpleSymbol add(SimpleSymbol other) {
@@ -64,6 +108,13 @@ public class SimpleSymbol {
 
     @Override
     public String toString() {
-        return String.format("%s * %s", factor, symbol);
+        if (power.equals(new SmartNum(1)) && factor.equals(new SmartNum(1)))
+           return String.valueOf(symbol);
+        else if (power.equals(new SmartNum(1)))
+            return String.format("(%s) * %s", factor, symbol);
+        else if (factor.equals(new SmartNum(1)))
+            return String.format("%s ^ (%s)", symbol, power);
+        else
+            return String.format("(%s) * %s ^ (%s)", factor, symbol, power);
     }
 }
